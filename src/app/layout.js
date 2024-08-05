bb804bece99e33697f7e7d3d497c2402feb90f96
yasmen-ahmed <yasmenahmedmuhamed@gmail.com>
2024-08-05 14:53:18 +0300

    modify the sidbar component

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <div className="flex justify-between mx-auto max-w-6xl ">
-          <div>
+          <div className="hidden sm:inline border-r h-screen">
             <Sidebar/>
           </div>
           <div>
