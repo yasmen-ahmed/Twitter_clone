bb6d8317c9fc7b457833a3c36c7e4fa8a6d5c35a
yasmen-ahmed <yasmenahmedmuhamed@gmail.com>
2024-08-05 12:51:11 +0300

    create page and routes as well as components

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,34 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Sidebar from '@/components/Sidebar'
+import News from '@/components/News'
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Twitter Clone",
-  description: "Generate twitter clone",
+  title: "X Clone",
+  description: "Generate twitter clone website with Next js ",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex justify-between mx-auto max-w-6xl ">
+          <div>
+            <Sidebar/>
+          </div>
+          <div>
+          {children}
+          </div>
+          <div>
+            <News/>
+          </div>
+       
+       
+
+        </div>
+        </body>
     </html>
   );
 }
